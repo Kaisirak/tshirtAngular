69ddb9ac3ef8ce5343cd0786bc4ebd1fa6df0077
Jules Girodon <julesgirodon@Juless-iMac.local>
2015-04-14 14:21:21 -0700

    test prod 8

--- js/controllers.js
+++ js/controllers.js
@@ -241,15 +241,42 @@ function pathalize(name) {
 		};
 	}]);
 
-	app.filter('uniqueColor', function() {
-   var prevVal = null;
-   return function(input) {
-     if (prevVal !== input) {
-       prevVal = input;
-       return prevVal;
-     }
-   };
-});
+	app.filter('unique', ['$parse', function ($parse) {
+  'use strict';
+
+  return function (items, filterOn) {
+
+    if (filterOn === false) {
+      return items;
+    }
+
+    if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+      var newItems = [],
+        get = angular.isString(filterOn) ? $parse(filterOn) : function (item) { return item; };
+
+      var extractValueToCompare = function (item) {
+        return angular.isObject(item) ? get(item) : item;
+      };
+
+      angular.forEach(items, function (item) {
+        var isDuplicate = false;
+
+        for (var i = 0; i < newItems.length; i++) {
+          if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+            isDuplicate = true;
+            break;
+          }
+        }
+        if (!isDuplicate) {
+          newItems.push(item);
+        }
+
+      });
+      items = newItems;
+    }
+    return items;
+  	};
+	}]);
 
 	app.controller('DesignerController', ["$http", "$routeParams", "$scope", function($http,$routeParams,$scope) {
 
